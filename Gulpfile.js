67b20874ce319c1140cd5ad2e77957556d47684d
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-03-21 01:28:58 -0300

    Inserindo uma estrutura

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,11 @@
-const { series } = require('gulp');
+const gulp = require('gulp');
 
+function default_func(callback) {
+    console.log("Executando via Gulp!")
+    callback();
+}
+
+exports.default = default_func;
+
+// Processando tarefas em serie
+// exports.default = gulp.series(funcao_aqui)
